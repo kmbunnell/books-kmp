cf32199de44b1c48cd7cd7b433d8fe3189d791b0
Kristina Bunnell <mrskbunnell@gmail.com>
2026-04-15 15:34:25 -0400

    Create shared-testing module for test fakes to reduce duplication

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -102,6 +102,7 @@ kotlin {
             implementation(libs.kotlin.test)
             implementation(libs.kotlinx.coroutines.test)
             implementation(libs.turbine)
+            implementation(projects.sharedTesting)
         }
         androidUnitTest.dependencies {
             implementation(libs.robolectric)
