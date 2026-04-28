3703ed4910f18fc4b7c93a6907b15288d32775d0
Kristina Bunnell <mrskbunnell@gmail.com>
2026-04-28 13:24:21 -0400

    refactor to typed navigation

---
+++ composeApp/src/commonMain/kotlin/com/example/books_kmp/Route.kt
@@ -0,0 +1,14 @@
+package com.example.books_kmp
+
+import kotlinx.serialization.Serializable
+
+sealed interface Route {
+    @Serializable data object Splash : Route
+    @Serializable data object SignIn : Route
+    @Serializable data object SignUp : Route
+    @Serializable data object Library : Route
+    @Serializable data object AddBook : Route
+    @Serializable data object ManualEntry : Route
+    @Serializable data object TagManagement : Route
+    @Serializable data class BookDetail(val bookId: String) : Route
+}
