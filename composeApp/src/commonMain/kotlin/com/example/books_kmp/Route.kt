3297547532c3ce6a5cd257b9492b5de2996d8e38
Kristina Bunnell <mrskbunnell@gmail.com>
2026-04-28 13:27:03 -0400

    Formatting

--- composeApp/src/commonMain/kotlin/com/example/books_kmp/Route.kt
+++ composeApp/src/commonMain/kotlin/com/example/books_kmp/Route.kt
@@ -4,11 +4,18 @@ import kotlinx.serialization.Serializable
 
 sealed interface Route {
     @Serializable data object Splash : Route
+
     @Serializable data object SignIn : Route
+
     @Serializable data object SignUp : Route
+
     @Serializable data object Library : Route
+
     @Serializable data object AddBook : Route
+
     @Serializable data object ManualEntry : Route
+
     @Serializable data object TagManagement : Route
+
     @Serializable data class BookDetail(val bookId: String) : Route
 }
