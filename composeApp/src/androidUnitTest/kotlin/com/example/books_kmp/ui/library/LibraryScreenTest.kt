f2aee01b96e043051df35ca60466ca7933892b7b
Kristina Bunnell <mrskbunnell@gmail.com>
2026-04-28 16:26:49 -0400

    library screen formatting

--- composeApp/src/androidUnitTest/kotlin/com/example/books_kmp/ui/library/LibraryScreenTest.kt
+++ composeApp/src/androidUnitTest/kotlin/com/example/books_kmp/ui/library/LibraryScreenTest.kt
@@ -6,7 +6,6 @@ import androidx.compose.ui.test.assertIsNotSelected
 import androidx.compose.ui.test.assertIsSelected
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithTag
-import androidx.compose.ui.test.onNodeWithText
 import androidx.compose.ui.test.performClick
 import com.example.books_kmp.domain.model.Tag
 import com.example.books_kmp.ui.TestTags
