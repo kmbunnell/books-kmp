2b75f845ea8395500340c1f40eef963d45873f1d
Kristina Bunnell <mrskbunnell@gmail.com>
2026-05-06 16:20:04 -0400

    make isbn nullable for booklookupdata

--- shared/src/commonMain/kotlin/com/example/books_kmp/domain/model/BookLookupData.kt
+++ shared/src/commonMain/kotlin/com/example/books_kmp/domain/model/BookLookupData.kt
@@ -1,7 +1,7 @@
 package com.example.books_kmp.domain.model
 
 data class BookLookupData(
-    val isbn: String,
+    val isbn: String?,
     val title: String,
     val authors: List<String>,
     val coverImageUrl: String?,
