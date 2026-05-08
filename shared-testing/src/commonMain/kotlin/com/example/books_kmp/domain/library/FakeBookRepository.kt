d0f3ba7db41087096c0376d827b878ec3b78623e
Kristina Bunnell <mrskbunnell@gmail.com>
2026-05-08 09:32:37 -0400

    add dismiss to dup dialog, contentDesc to back button, lowercase title for dupl search

--- shared-testing/src/commonMain/kotlin/com/example/books_kmp/domain/library/FakeBookRepository.kt
+++ shared-testing/src/commonMain/kotlin/com/example/books_kmp/domain/library/FakeBookRepository.kt
@@ -100,7 +100,7 @@ class FakeBookRepository(
     }
 
     override suspend fun findBookByTitle(title: String): Result<Book?, BookRepositoryError> =
-        Result.Success(books.find { it.title.equals(title, ignoreCase = true) })
+        Result.Success(books.find { it.title.lowercase().trim() == title })
 
     override suspend fun isbnExists(isbn: String?): Result<Boolean, BookRepositoryError> {
         if (isbnExistsShouldFail) return Result.Failure(BookRepositoryError.NetworkError)
