7f20fa0e503bb58bbe23e1d6e682b082725f82b2
Kristina Bunnell <mrskbunnell@gmail.com>
2026-05-31 13:44:26 -0400

    formatting

--- composeApp/src/commonMain/kotlin/com/example/books_kmp/ui/addbook/AddBookScreen.kt
+++ composeApp/src/commonMain/kotlin/com/example/books_kmp/ui/addbook/AddBookScreen.kt
@@ -22,12 +22,12 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SegmentedButton
 import androidx.compose.material3.SegmentedButtonDefaults
 import androidx.compose.material3.SingleChoiceSegmentedButtonRow
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
