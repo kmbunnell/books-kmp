51f5838ef64c20325568542e635ea0cf2ce0266c
Kristina Bunnell <bonehilldev@gmail.com>
2026-07-29 13:02:26 -0400

    iosX64() added to shared, shared-testing, and composeApp's build.gradle to build on current intel mac

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -123,7 +123,8 @@ kotlin {
 
     listOf(
         iosArm64(),
-        iosSimulatorArm64()
+        iosSimulatorArm64(),
+        iosX64()
     ).forEach { iosTarget ->
         iosTarget.binaries.framework {
             baseName = "ComposeApp"
--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -24,6 +24,7 @@ kotlin {
     }
     iosArm64()
     iosSimulatorArm64()
+    iosX64()
     jvm("desktop")
 
     sourceSets {
